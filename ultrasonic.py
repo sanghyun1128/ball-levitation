45ac9964d493e77bf60b3b2d917d3bfdc2771444
sanghyun1128 <sanghyun1128@gmail.com>
2022-11-21 22:08:17 +0900

    📰 Feat : Ultrasonic sensor implementation
    setup and distance function implementation

---
+++ ultrasonic.py
@@ -0,0 +1,34 @@
+import RPi.GPIO as GPIO
+import time
+
+class Ultrasonic:
+    def __init__(self, trigPinNum, echoPinNum):
+        self.trigPinNum = trigPinNum
+        self.echoPinNum = echoPinNum
+        
+    def setup(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.trigPinNum, GPIO.OUT)
+        GPIO.setup(self.echoPinNum, GPIO.IN)
+        
+    def getDistance(self):
+        GPIO.output(self.trigPinNum, 0)
+        time.sleep(0.000002)
+
+        GPIO.output(self.trigPinNum, 1)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, 0)
+
+        while GPIO.input(self.echoPinNum) == 0:
+            a = 0
+        time1 = time.time()
+        while GPIO.input(self.echoPinNum) == 1:
+            a = 1
+        time2 = time.time()
+
+        during = time2 - time1
+        return during * 340 / 2 * 100
+    
+    def destroy():
+        GPIO.cleanup()
+
