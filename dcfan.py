9e445ab84498a674b3d3fe685bf713ef6d4f46b6
sanghyun1128 <sanghyun1128@gmail.com>
2022-11-21 22:54:41 +0900

    📰 Feat :  DC Fan implementation
    setup and pwm fan control function implementation

---
+++ dcfan.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+
+class Fan:
+    def __init__(self, fanPinNum):
+        self.fanPinNum = fanPinNum
+        self.fanSpeed = 0
+        
+    def setup(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.fanPinNum, GPIO.OUT)
+        
+    def setFanSpeed(self, fanSpeed):
+        self.fanSpeed = fanSpeed
+        
+    def startFan(self):
+        while(True):
+            for i in range(self.fanSpeed):
+                GPIO.output(self.fanPinNum, True)
+                time.sleep(0.01)
+            for j in range(100-self.fanSpeed):
+                GPIO.output(self.fanPinNum, False)
+                time.sleep(0.01)
+                
+    def destory():
+        GPIO.cleanup()
