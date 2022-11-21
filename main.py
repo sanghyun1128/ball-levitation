e7a54defdb6b5336313ee3eefac220320e44cc8f
sanghyun1128 <sanghyun1128@gmail.com>
2022-11-21 23:50:57 +0900

    📰 Feat : Make main.py for test

---
+++ main.py
@@ -0,0 +1,27 @@
+from fan import Fan
+from ultrasonic import Ultrasonic
+
+fan = Fan(23)
+ultrasonic = Ultrasonic(17, 18)
+
+fan.setup()
+ultrasonic.setup()
+
+desireDistance = 20
+
+currentSpeed = 50
+fan.setFanSpeed(currentSpeed)
+
+try:
+    while(1):
+        fan.pwmOnFan10ms()
+        distance = ultrasonic.getDistance()
+        if (distance < desireDistance):
+            currentSpeed -= 5
+            fan.setFanSpeed(currentSpeed)
+        else :
+            currentSpeed += 5
+            fan.setFanSpeed(currentSpeed)
+except KeyboardInterrupt:
+    fan.destory()
+    ultrasonic.destroy()
