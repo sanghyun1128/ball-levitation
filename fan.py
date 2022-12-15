345fe43971cb576321394904ec13a69023cfc775
sanghyun1128 <sanghyun1128@gmail.com>
2022-12-16 03:15:58 +0900

    📝 Fix : Add code for pid control

--- fan.py
+++ fan.py
@@ -11,16 +11,28 @@ class Fan:
         GPIO.setwarnings(False)
         GPIO.setup(self.fanPinNum, GPIO.OUT)
         
+    def getSpeed(self):
+        return self.fanSpeed
     def setFanSpeed(self, fanSpeed):
         self.fanSpeed = fanSpeed
+    
+    def addFanSpeed(self, num):
+        self.fanSpeed += num
+        if self.fanSpeed > 100:
+            self.fanSpeed = 100
+    
+    def minusFanSpeed(self, num):
+        self.fanSpeed -= num
+        if self.fanSpeed < 0:
+            self.fanSpeed = 0
         
-    def pwmOnFan10ms(self):
+    def pwmOnFan100ms(self):
+        GPIO.output(self.fanPinNum, True)
         for i in range(self.fanSpeed):
-            GPIO.output(self.fanPinNum, True)
-            time.sleep(0.0001)
+            time.sleep(0.001)
+        GPIO.output(self.fanPinNum, False)
         for j in range(100-self.fanSpeed):
-            GPIO.output(self.fanPinNum, False)
-            time.sleep(0.0001)
+            time.sleep(0.001)
                 
     def destory(self):
         GPIO.cleanup()
--- main.py
+++ main.py
@@ -1,32 +1,25 @@
 from fan import Fan
 from ultrasonic import Ultrasonic
+from pid import PID_Control
 import RPi.GPIO as GPIO
 GPIO.cleanup()
 
 fan = Fan(23)
 ultrasonic = Ultrasonic(17, 18)
+pid = PID_Control(0.1, 0, 100, 1, 1, 1)
 
 fan.setup()
 ultrasonic.setup()
 
 desireDistance = 20
 
-currentSpeed = 50
-fan.setFanSpeed(currentSpeed)
-
+prevErr = 20
 try:
     while(1):
-        fan.pwmOnFan10ms()
-        distance = ultrasonic.getDistance()
-        print("Distance : " + str(distance) + " / " + "Speed : " + str(currentSpeed))
-        if (distance < desireDistance):
-            if currentSpeed >= 5:
-                currentSpeed -= 5
-            fan.setFanSpeed(currentSpeed)
-        else :
-            if currentSpeed <= 95:
-                currentSpeed += 5
-            fan.setFanSpeed(currentSpeed)
+        currdistance = ultrasonic.getDistance()
+        fan.setFanSpeed(pid.calc(currdistance, desireDistance))
+        print("Distance : " + str(currdistance) + " / " + "Speed : " + str(fan.getSpeed()))
+        fan.pwmOnFan100ms()
 except KeyboardInterrupt:
     fan.destory()
     ultrasonic.destroy()
